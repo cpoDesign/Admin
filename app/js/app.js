dd12eee46e31227091f99d847cef323f20fd0dd1
6872 <pvl.svarc@gmail.com>
2014-08-13 17:51:36 +0100

    Added constants and access level to routes.

--- app/js/app.js
+++ app/js/app.js
@@ -7,27 +7,37 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/login.html', controller: 'LoginCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/tables', {templateUrl: 'partials/tables.html', controller: 'TablesCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardCtrl', access_level: ACCESS_LEVELS.pub});
-  $routeProvider.when('/forms', {templateUrl: 'partials/forms.html', controller: 'FormsCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/charts', {templateUrl: 'partials/charts.html', controller: 'ChartsCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/grid', {templateUrl: 'partials/grid.html', controller: 'GridCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/elements', {templateUrl: 'partials/elements.html', controller: 'ElementsCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/elements', {templateUrl: 'partials/elements.html', controller: 'ElementsCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/messages', {templateUrl: 'partials/messages.html', controller: 'MessagesCtrl', access_level: ACCESS_LEVELS.user});
-  $routeProvider.when('/messages/:messageId',
-      {templateUrl: 'partials/messageDetail.html', controller: 'MessageDetailCtrl',
-        resolve: {
-            message: function (messagesSvc, $route) {
-                return messagesSvc.getMessage($route.current.params.messageId);
-            }
-        },
-          access_level: ACCESS_LEVELS.user
-      });
+  'myApp.controllers',
+])
+    .constant('ACCESS_LEVELS',{
+
+        pub:1,
+        user:2,
+        admin:9
+
+    })
+
+ .config(['$routeProvider','ACCESS_LEVELS', function($routeProvider, ACCESS_LEVELS) {
+
+   $routeProvider.when('/',{templateUrl: 'partials/login.html',controller: 'LoginCtrl',access_level: ACCESS_LEVELS.pub});
+   $routeProvider.when('/tables', {templateUrl: 'partials/tables.html', controller: 'TablesCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/forms', {templateUrl: 'partials/forms.html', controller: 'FormsCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/charts', {templateUrl: 'partials/charts.html', controller: 'ChartsCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/grid', {templateUrl: 'partials/grid.html', controller: 'GridCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/elements', {templateUrl: 'partials/elements.html', controller: 'ElementsCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/elements', {templateUrl: 'partials/elements.html', controller: 'ElementsCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/messages', {templateUrl: 'partials/messages.html', controller: 'MessagesCtrl', access_level: ACCESS_LEVELS.user});
+   $routeProvider.when('/messages/:messageId',{
+                templateUrl: 'partials/messageDetail.html',
+                controller: 'MessageDetailCtrl',
+                access_level: 2,
+                resolve: {
+                    message: function (messagesSvc, $route) {
+                        return messagesSvc.getMessage($route.current.params.messageId);
+                    }
+                }
+            });
 
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
