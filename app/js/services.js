1221bb3ef44e85c74384d4e18a57da6ce214cc8b
6872 <pvl.svarc@gmail.com>
2014-08-13 16:42:29 +0100

    Completed default behavior for authentication

--- app/js/services.js
+++ app/js/services.js
@@ -6,8 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-    .value('version', '0.1');
-
+    .value('version', '0.1.1');
 
 angular.module('myApp.services', [])
     .factory('messagesSvc',function messagesSvc() {
@@ -77,6 +76,36 @@ angular.module('myApp.services', [])
 angular.module('myApp',[])
     .config(function($httpProvider){
         // build our interceptor here
+        var interceptor = function($q, $rootScope, Auth){
+            return{
+              'response': function(resp){
+                  if(resp.config.url =='/api/login'){
+                      // assuming that our api server response with following data: {token: 'AUTH_TOKEN'}
+                      Auth.setToken(resp.data.token);
+                  }
+              } ,
+                'responseError': function(rejection){
+                    switch (rejection.status){
+                        case 401:
+                            if(rejection.config.url !=='api/login')
+                            $rootScope.$broadcast('auth:loginRequired');
+                            break;
+                        case 403:
+                            $rootScope.$broadcast('auth:forbidden');
+                            break;
+                        case 404:
+                            $rootScope.$broadcast('page:notFound');
+                            break;
+                        case 500:
+                            $rootScope.$broadcast('server:error');
+                            break;
+                    }
+                    return $q.reject(rejection)
+                }
+            }
+        };
+
+        $httpProvider.interceptors.push(interceptor());
     });
 
 
